25a00e71a65eb69f464250dedb96846efb872c14
belunatic <abelluvyou@yahoo.com>
2024-12-18 13:04:48 -0800

    my first upload

---
+++ data.js
@@ -0,0 +1,44 @@
+module.exports = [
+	{
+		id: 1,
+		name: "John Doe",
+		age: 30,
+		address: "123 Main St, Anytown, USA",
+		phone_number: "555-1234",
+	},
+	{
+		id: 2,
+		name: "Jane Smith",
+		age: 25,
+		address: "456 Oak St, Anytown, USA",
+		phone_number: "555-5678",
+	},
+	{
+		id: 3,
+		name: "Emily Johnson",
+		age: 35,
+		address: "789 Pine St, Anytown, USA",
+		phone_number: "555-9012",
+	},
+	{
+		id: 4,
+		name: "Michael Brown",
+		age: 40,
+		address: "101 Maple Ave, Anytown, USA",
+		phone_number: "555-3456",
+	},
+	{
+		id: 5,
+		name: "Sarah Davis",
+		age: 28,
+		address: "202 Elm St, Anytown, USA",
+		phone_number: "555-7890",
+	},
+	{
+		id: 6,
+		name: "David Wilson",
+		age: 33,
+		address: "303 Pine Blvd, Anytown, USA",
+		phone_number: "555-2345",
+	},
+];
---
+++ server.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const cors = require("cors");
+//data
+let data = require("./data");
+const { json } = require("body-parser");
+const app = express();
+//middleware
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+	res.send("<h1>Welcome to my API</h1>");
+});
+app.get("/person", (req, res) => {
+	res.json(data);
+});
+
+app.get("/person/:id", (req, res) => {
+	const id = Number(req.params.id);
+	personData = data.filter((person) => person.id === id);
+	console.log(personData === false);
+	if (personData.length === 0) {
+		res.status(404).send("That person does not exists");
+	} else {
+		res.json(personData);
+	}
+});
+
+app.delete("/person/:id", (req, res) => {
+	const id = req.params.id;
+	personData = data.map((person) => person.id !== id);
+	res.status(204).json(personData);
+});
+
+app.post("/person", (req, res) => {
+	const body = req.body;
+	if (!req.body.name) {
+		return res.status(404).json({ Error: "Name is required" });
+	} else if (!req.body.age) {
+		return res.status(404).json({ Error: "Age is required" });
+	}
+
+	const personData = {
+		id: data.length + 1,
+		name: req.body.name,
+		age: req.body.age,
+		address: req.body.address || "NA",
+		phone_number: req.body.phone_number || "NA",
+	};
+
+	data = [...data, personData];
+
+	res.json(data);
+});
+
+app.listen(process.env.PORT || 5000, () => {
+	console.log("I am listening, so no worries your secret is safe with me");
+});
